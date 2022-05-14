8164d77bbeb3e463e3f6282261759277e57c24ea
DaboRestrepo <daborestrepo@gmail.com>
2022-05-14 12:32:56 -0500

    import models instead FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,8 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
-from models.engine.file_storage import FileStorage
+from sqlalchemy import Column, String, DateTime
+import models
 
 Base = declarative_base()
 
@@ -18,7 +18,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -39,8 +38,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -50,10 +49,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
+        if '_sa_instance_state' in self.__dict__:
             del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
         """delete the current instance from the storage"""
-        FileStorage.storage.delete(self)
+        models.storage.delete()
