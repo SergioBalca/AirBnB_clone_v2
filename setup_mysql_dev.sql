80de9539401536d6aaee0a1210720a5e00030bc3
SergioBalca <3896@holbertonschool.com>
2022-05-16 19:21:17 -0700

    error with grant privileges fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,6 @@
 -- Prepares a MySQL server
+-- creates a database
+-- Creates a new user with all privileges on the database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
