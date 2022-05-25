b0d6e4a8d8446076c2c8e0bef6460031aa278d30
SergioBalca <3896@holbertonschool.com>
2022-05-25 11:50:51 -0500

    web application that fetches data from the storage engine

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display an HTML page"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session():
+    """Method to handle tear_down_appcontext"""
+    storage.close()
